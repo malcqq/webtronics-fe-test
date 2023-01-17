f9e935466b26ad3854ef7a741c0753459b29fa3d
malcqq <malcqq@gmail.com>
2023-01-17 03:17:53 +0300

    add component: Htag

---
+++ src/components/Htag/index.tsx
@@ -0,0 +1,18 @@
+import { IHTagProps } from './Props';
+import styles from './Styles.module.css';
+
+const Htag = ({ tag, children }: IHTagProps): JSX.Element => {
+  switch (tag) {
+    case 'h1':
+      return <h1 className={styles.h1}>{children}</h1>;
+    case 'h2':
+      return <h2 className={styles.h2}>{children}</h2>;
+    case 'h3':
+      return <h3 className={styles.h3}>{children}</h3>;
+    default:
+      return <></>;
+  }
+  return <></>;
+};
+
+export default Htag;
